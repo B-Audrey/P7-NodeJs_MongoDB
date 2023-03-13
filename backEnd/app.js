802dd6431fd5708b9cee8aa2effebf80d85895f2
B-Audrey <bonis.audreyy@gmail.com>
2023-03-13 12:03:32 +0100

    Ajout des premières routes

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,5 +1,43 @@
+//importe Express
 const express = require('express');
 
 const app = express();
+//importe mongoose
+const mongoose = require('mongoose');
 
-module.export = app;
+const booksRoutes = require('./routes/books');
+const authRoutes = require('./routes/auth');
+
+//lie à MongoDB
+mongoose.connect('mongodb+srv://AudeRey:yFAc2RXaOq5ekyKl@cluster0.x4nhhec.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+//CORS - autorise l'accès à tous
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  // app.use('/api/books', (req, res, next) => {
+  //   const token = req.headers('Authorization').split(' ')[1];
+  //   if(token.isExpired){
+  //       next({code: 401, message: 'Unauthorized'});
+  //   }
+  //   if(token.isExpired){
+  //     return res.status(401).json({message: 'Unauthorized'});
+  //   }
+  //   next();
+  // })
+
+//renvoie du express en JSON
+app.use(express.json());
+app.use('/api/books', booksRoutes);
+app.use('/api/auth', authRoutes);
+
+module.exports = app;
